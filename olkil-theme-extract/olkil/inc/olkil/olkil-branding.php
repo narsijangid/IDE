fa99abde208dc7d01945b92055188247f2d0b189
narsi-rinovea <@narsijangid76D>
2026-08-26 02:43:05 +0530

    minor

--- olkil-theme-extract/olkil/inc/olkil/olkil-branding.php
+++ olkil-theme-extract/olkil/inc/olkil/olkil-branding.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'OLKIL_VERSION', '1.4.5' );
+define( 'OLKIL_VERSION', '1.4.9' );
 /** Desktop installer version (matches ide-electron/product.json). */
 define( 'OLKIL_APP_VERSION', '1.3.19' );
 define( 'OLKIL_DIR', trailingslashit( get_template_directory() ) );
